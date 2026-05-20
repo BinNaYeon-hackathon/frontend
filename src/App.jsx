133dfa72a6c57b50d1420d6c39e4a78bdff35faf
ninum12 <ninum@sookmyung.ac.kr>
2026-05-20 11:17:22 +0900

    feat: logo navigation 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+  Link,
+} from "react-router-dom";
+
+import logo from "./assets/logo-post-for-you.svg";
 
 import HomePage from "./pages/HomePage";
 import BrandPage from "./pages/BrandPage";
 import CreatePage from "./pages/CreatePage";
 import PreviewPage from "./pages/PreviewPage";
 
-function App() {
+function Layout() {
+  const location = useLocation();
+
+  const isHome = location.pathname === "/";
+
   return (
-    <BrowserRouter>
+    <div className="page">
+      {!isHome && (
+        <Link to="/">
+          <img src={logo} alt="logo" className="logo" />
+        </Link>
+      )}
+
       <nav
         style={{
           display: "flex",
@@ -27,6 +45,14 @@ function App() {
         <Route path="/create" element={<CreatePage />} />
         <Route path="/preview" element={<PreviewPage />} />
       </Routes>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Layout />
     </BrowserRouter>
   );
 }
