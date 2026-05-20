97157590fe2cf1d3a846f3a3337d43d1593a248c
ninum12 <ninum@sookmyung.ac.kr>
2026-05-20 11:59:52 +0900

    feat: preview 페이지의 tab-bar 구현

--- src/pages/PreviewPage.jsx
+++ src/pages/PreviewPage.jsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+import "./PreviewPage.css";
+
 function PreviewPage() {
-  return <h1>생성된 게시글 미리보기 화면</h1>;
+  const [tab, setTab] = useState("instagram");
+
+  return (
+    <div className="preview-page">
+      <div className="preview-header">
+        <h2>게시물 생성 완료</h2>
+        <p>클릭하여 텍스트를 수정하세요</p>
+      </div>
+      <div className="preview-card">
+        <div className="tab-bar">
+          <button
+            className={tab === "instagram" ? "tab-button active" : "tab-button"}
+            onClick={() => setTab("instagram")}
+          >
+            Instagram
+          </button>
+
+          <button
+            className={tab === "x" ? "tab-button active" : "tab-button"}
+            onClick={() => setTab("x")}
+          >
+            X
+          </button>
+
+          <button
+            className={tab === "linkedin" ? "tab-button active" : "tab-button"}
+            onClick={() => setTab("linkedin")}
+          >
+            LinkedIn
+          </button>
+        </div>
+        <div className="preview-content">
+          {tab === "instagram" && (
+            <div>
+              <h2>Instagram Preview</h2>
+            </div>
+          )}
+
+          {tab === "x" && (
+            <div>
+              <h2>X Preview</h2>
+            </div>
+          )}
+
+          {tab === "linkedin" && (
+            <div>
+              <h2>LinkedIn Preview</h2>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PreviewPage;
